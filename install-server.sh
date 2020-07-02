4f195c29842126c204a8f0eaf4b85bfef1393584
Edoardo Zanoni <edoardo.zanoni@gambabruno.it>
2020-07-02 15:39:26 +0200

    Aggiunti echo di dettaglio

--- install-server.sh
+++ install-server.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
 
 ## Dependencies installation
+echo "Dependencies installation"
 sudo apt-get install -y software-properties-common apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
 sudo apt autoremove -y
 
 ## Disabling ModemManager (if exist) for compatibility
+echo "Disabling ModemManager (if exist) for compatibility"
 sudo systemctl disable ModemManager
 sudo systemctl stop ModemManager
 
 ## Install Docker CE
-sudo curl -fsSL get.docker.com | sudo sh
+echo "Install Docker CE"
+curl -fsSL get.docker.com | sudo sh
 
 ## Install Supervised Home ASsistant
-sudo curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | sudo bash -s
+echo "Install Supervised Home ASsistant"
+curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | sudo bash -s
 
 ## Docker-compose installation as a docker container
+echo "Docker-compose installation as a docker container"
 sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 cd ~
 homeDir=$(pwd)
 ## Creation of the services' dir
+echo "Creation of the services' dir"
 if [ ! -d ~/.services ]
 then
 	mkdir ~/.services
@@ -28,6 +34,7 @@ fi
 cd ~/.services
 
 ## Importing and setting docker-compose.yml
+echo "Importing and setting docker-compose.yml"
 >"${homeDir}/docker-compose.yml" curl -sL "https://raw.githubusercontent.com/Ryther/homeserver/master/docker-compose.yml"
 sed -i 's|{homeDir}|'"${homeDir}"'|' "${homeDir}/docker-compose.yml"
 
